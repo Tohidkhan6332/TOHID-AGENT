cae8c0199ce7c292e63e659c482f4c96164d6204
Tohid Khan <tohidkhan9050482152@gmail.com>
2026-09-20 19:01:11 +0530

    chore: bump TOHID-AGENT to V7.1

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports={
   developer:"Tohid",
   brand:"TOHID-AGENT",
-  version:"7.0.0",
+  version:"7.1.0",
   openaiKey:process.env.OPENAI_API_KEY||"",
   openaiModel:process.env.OPENAI_MODEL||"gpt-5.6-luna",
   model:process.env.OPENAI_MODEL||"gpt-5.6-luna",
