b52f11cf2e8c2813c43850aa21bd82b963d6ade4
Tohid Khan <tohidkhan9050482152@gmail.com>
2026-09-21 20:49:33 +0530

    feat: add configurable bot UI mode

--- config.js
+++ config.js
@@ -57,6 +57,7 @@ module.exports={
   taskVerification:process.env.AGENT_TASK_VERIFICATION!=="false",
   channelLink:process.env.CHANNEL_LINK||"https://whatsapp.com/channel/0029VaGyP933bbVC7G0x0i2T",
   interactiveButtonsEnabled:process.env.INTERACTIVE_BUTTONS_ENABLED!=="false",
+  defaultUIMode:(process.env.BOT_UI_MODE||"both").toLowerCase(),
   baileysExtrasEnabled:process.env.BAILEYS_EXTRAS_ENABLED!=="false",
   debug:process.env.DEBUG==="true",
   githubEnabled:process.env.GITHUB_AGENT_ENABLED!=="false",
