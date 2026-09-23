dbf9956f3367a3fcce081d8920a86a29f02e349d
Tohid Khan <tohidkhan9050482152@gmail.com>
2026-09-23 15:47:49 +0530

    chore: add private deployment secrets template

---
+++ secrets.example.js
@@ -0,0 +1,13 @@
+// Copy this file to secrets.js on your private deployment.
+// Keep secrets.js out of Git. Do NOT paste API keys into config.js or source files.
+module.exports=Object.freeze({
+  openaiApiKey:"",
+  geminiApiKey:"",
+  githubToken:"",
+  herokuApiKey:"",
+  vercelToken:"",
+  renderApiKey:"",
+  koyebApiToken:"",
+  telegramBotToken:"",
+  adminPanelToken:""
+});
